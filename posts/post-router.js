b71d7d568dfd4372484ba4f8164acfc519a36245
abhitsahota <abhit.sahota@gmail.com>
2020-05-19 23:22:10 -0400

    added get request

--- posts/post-router.js
+++ posts/post-router.js
@@ -9,8 +9,14 @@ router.get('/', (req, res) => {
 
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const result = await knex('posts').where({ id })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ msg: 'problem with db', error: err })
+    }
 });
 
 router.post('/', (req, res) => {
